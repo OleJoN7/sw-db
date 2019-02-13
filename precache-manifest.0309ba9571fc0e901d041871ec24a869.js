1818a454db0e918f1163edd8236324f3a0c111ad
oleg <oleb140888@gmail.com>
2019-02-13 15:15:01 +0200

    Updates

---
+++ precache-manifest.0309ba9571fc0e901d041871ec24a869.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fcafbd96bf614d060b3f18ace43d140",
+    "url": "/sw-db/static/media/starships.6fcafbd9.gif"
+  },
+  {
+    "revision": "cb51ee6f7a23983135a812c04f476eb8",
+    "url": "/sw-db/static/media/planets.cb51ee6f.gif"
+  },
+  {
+    "revision": "732cb37d7984d1788f25e23b5f99885f",
+    "url": "/sw-db/static/media/characters.732cb37d.gif"
+  },
+  {
+    "revision": "9b3a2197ebd51f7f4230",
+    "url": "/sw-db/static/js/runtime~main.9b3a2197.js"
+  },
+  {
+    "revision": "de75498ae307b69ef58d",
+    "url": "/sw-db/static/js/main.de75498a.chunk.js"
+  },
+  {
+    "revision": "0a519d08d8d9dbe6de8e",
+    "url": "/sw-db/static/js/1.0a519d08.chunk.js"
+  },
+  {
+    "revision": "de75498ae307b69ef58d",
+    "url": "/sw-db/static/css/main.9cd6ce3e.chunk.css"
+  },
+  {
+    "revision": "233f546ffcf38bc9d53fe200b6778e96",
+    "url": "/sw-db/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/sw-db/precache-manifest.0309ba9571fc0e901d041871ec24a869.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/sw-db/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
